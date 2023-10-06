e557a7db4205db8ae2d4fec7ef92fef6eb410aae
jashuart <joelshuart@gmail.com>
2023-10-06 12:23:04 -0400

    improved media zoom to cover videos

--- js/add-media-zoom.js
+++ js/add-media-zoom.js
@@ -1,11 +1,35 @@
-const imgs = document.querySelectorAll('.scroll-container img');
+const scrolls = document.querySelectorAll('.scroll-container');
 const overlay = document.querySelector('#media-zoom');
 
-imgs.forEach(img => {
-    console.log(img);
-  img.addEventListener('click', function() {
-    overlay.style.backgroundImage = 'url(' + img.src + ')';
-    overlay.style.display = 'block';
-    console.log(img);
+let allMedia = [];
+scrolls.forEach(scroll => { 
+    allMedia = allMedia.concat([...scroll.children]); 
+});
+
+console.log(allMedia);
+
+overlay.addEventListener('click', function(){
+    overlay.style.opacity = 0;
+    //wait until css tranistion finishes before chaning display mode
+    setTimeout(function(){ overlay.style.display = 'none'; }, 350);
+});
+
+allMedia.forEach(media => {
+  media.addEventListener('click', function() {
+    //set bg image to our overlay and give it the proper display
+    overlay.style.display = 'flex';
+    overlay.innerHTML = "";
+
+    let tCopy = media.cloneNode(true);
+    tCopy.style.height = '100vh';
+    tCopy.style.padding = '50px';
+    tCopy.style.paddingLeft = 'auto';
+    tCopy.style.paddingRight = 'auto';
+    overlay.appendChild(tCopy);
+    
+    //1ms delay so that the opacity css transition takes effect and isnt impacted by the display style change
+    setTimeout(function(){ 
+        overlay.style.opacity = 1;
+    }, 1);
   });
 });
