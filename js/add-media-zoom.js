6cb2eb56c6de385d7b757d34dbf1e05495df8077
jashuart <joelshuart@gmail.com>
2023-10-13 16:46:39 -0400

    updated media zoom to have next/prev buttons

--- js/add-media-zoom.js
+++ js/add-media-zoom.js
@@ -1,3 +1,5 @@
+var currentMediaIndex = null;
+
 //get all the media from the scroll containers
 const scrolls = document.querySelectorAll('.scroll-container');
 let allMedia = [];
@@ -7,29 +9,55 @@ scrolls.forEach(scroll => {
 
 //get overlay and give it the exit onclick callback
 const overlay = document.querySelector('#media-zoom');
+const target = document.querySelector('.media-zoom-target');
+
 overlay.addEventListener('click', function(){
     overlay.style.opacity = 0;
     setTimeout(function(){ overlay.style.display = 'none'; }, 350); //wait until css tranistion finishes before chaning display mode
 });
 
 //give all the media we gathered an onclick callback to make it the overlayed media
-allMedia.forEach(media => {
+for(let i = 0; i < allMedia.length; i++){
+  let media = allMedia[i];
   media.addEventListener('click', function() {
     //set bg image to our overlay and give it the proper display
     overlay.style.display = 'flex';
-    overlay.innerHTML = "";
-
-    let tCopy = media.cloneNode(true);
-    tCopy.style.width = '100%';
-    tCopy.style.padding = '50px';
-    tCopy.style.objectFit = "contain";
-    // tCopy.style.paddingLeft = 'auto';
-    // tCopy.style.paddingRight = 'auto';
-    overlay.appendChild(tCopy);
+    setImage(media, i);
     
     //1ms delay so that the opacity css transition takes effect and isnt impacted by the display style change
     setTimeout(function(){ 
         overlay.style.opacity = 1;
     }, 1);
   });
+}
+
+//add functionality to next and prev buttons
+const next = document.querySelector(".next");
+next.addEventListener('click', function(){
+  nextImage(1);
+  event.stopPropagation();
 });
+
+const prev = document.querySelector(".prev");
+prev.addEventListener('click', function(){
+  nextImage(-1);
+  event.stopPropagation();
+});
+
+nextImage = function(pIndexAdjust){
+  let tNewIndex = (currentMediaIndex + pIndexAdjust) % (allMedia.length);
+  setImage(allMedia[tNewIndex], tNewIndex);
+}
+
+setImage = function(pImage, pIndex){
+  currentMediaIndex = pIndex; //set index
+
+  target.innerHTML = ""; //clear current image
+
+  //set new image
+  let tCopy = pImage.cloneNode(true);
+  tCopy.style.width = '100%';
+  tCopy.style.padding = '30px';
+  tCopy.style.objectFit = "contain";
+  target.appendChild(tCopy);
+};
