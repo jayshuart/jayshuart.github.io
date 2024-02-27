6b32806655677249217252253a60888388cb8b0f
jashuart <joelshuart@gmail.com>
2024-02-26 21:45:06 -0500

    image updates, fix for title scalar when there is no media scroll

--- js/title-scaler.js
+++ js/title-scaler.js
@@ -1,6 +1,7 @@
 //get all the titles so we can resize them to always fit
 const titles = document.querySelectorAll('.title');
 const lineHeight = 50; 
+const lineHeightTall = 100;
 
 let onResize = function(){
   let tFontSize = 45;
@@ -10,12 +11,23 @@ let onResize = function(){
   let tFontScaler = 1;
   let tLocalFontSize = 0;
 
+  let tScroll = null;
+
   let i = 0;
   titles.forEach(title => {
     title.style.fontSize = "250%"; //reset to standard size for accurate size checking
 
-    tFontScaler = lineHeight / title.offsetHeight; 
-    tLocalFontSize = tFontSizeMin + (tFontScaler * tSizeDiff); 
+    tScroll = title.parentNode.parentNode.querySelector('.scroll-container');
+    if(tScroll != null){
+      tFontScaler = lineHeight / title.offsetHeight; 
+      tLocalFontSize = tFontSizeMin + (tFontScaler * tSizeDiff); 
+    }
+    else{
+      tFontScaler = (lineHeight * 10) / title.offsetHeight; 
+      tLocalFontSize = tFontSizeMin + ((tFontScaler * tSizeDiff) * 1); 
+    }
+
+    
 
     if(tLocalFontSize < tFontSize){
       title.style.fontSize = tLocalFontSize + "px";
