cafa34a10f77c7977d16ce862be6da18b7764891
jashuart <joelshuart@gmail.com>
2024-03-02 23:54:45 -0500

    imroved search

--- js/search-filter.js
+++ js/search-filter.js
@@ -6,15 +6,14 @@ const searchBar = document.getElementById('searchbar');
 document.getElementById("searchForm").addEventListener('submit', function(event){
   event.preventDefault();  //stop search form from refreshing page
   if (event.key === 'Enter' || event.keyCode === 13) {
-    //document.getElementById("id_of_button").click();
     onClickSearch();
-}
+  }
 }); 
 
 
 //set onlcick search func
 function onClickSearch(){
-  let input = searchBar.value; console.log(input);
+  let input = searchBar.value;
   if(input.trim() === "") { showAll(); }
   else { search(input); }
 }
@@ -24,20 +23,31 @@ function search(pSearchVal){
   pSearchVal = pSearchVal.toLowerCase().trim();
   
   cards.forEach( card => {
+    let tIsMatch = false;
+
     //check against game title, or workplace
+    let tWorkplace = card.querySelector(".workplace span");
+    let tTitle = card.querySelector(".title");
+
+    tIsMatch = compare(pSearchVal, tWorkplace.textContent) || 
+      compare(pSearchVal, tTitle.textContent);
 
     //get the list items in this cards details section
-    let tItems = card.querySelectorAll(".details li");
-
-    let tFitsSearchCriteria = false;
-    for(let i = 0; i < tItems.length; i++){
-      tFitsSearchCriteria = pSearchVal == tItems[i].textContent.toLowerCase().trim();
-      if(tFitsSearchCriteria) { break; }
+    if(!tIsMatch){
+      let tItems = card.querySelectorAll(".details li");
+      for(let i = 0; i < tItems.length; i++){
+        tIsMatch = compare(pSearchVal, tItems[i].textContent);
+        if(tIsMatch) { break; }
+      }
     }
 
-    card.style.display = tFitsSearchCriteria ? 
+    card.style.display = tIsMatch ? 
       "inline-block" : "none";
   });
+}
+
+function compare(pSearchVal, pInput){
+  return pInput.toLowerCase().trim().indexOf(pSearchVal) !== -1;
 }
 
 //resets all cards to be visible
